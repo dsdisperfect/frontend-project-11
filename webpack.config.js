ec885706507114d6dac0f1425b95f8e2e284b3d4
dsd90 <denisgavrilin9@icloud.com>
2023-10-18 14:55:31 +0300

    add minicss

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,5 @@
-// // Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
-
-// const isProduction = process.env.NODE_ENV == "production";
-
-// const stylesHandler = MiniCssExtractPlugin.loader;
-
-// const config = {
-//   entry: "./src/index.js",
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//   },
-//   devServer: {
-//     open: true,
-//     host: "localhost",
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: "index.html",
-//     }),
-
-//     new MiniCssExtractPlugin(),
-
-//     // Add your plugins here
-//     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/i,
-//         loader: "babel-loader",
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-//       },
-//       {
-//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-//         type: "asset",
-//       },
-
-//       // Add your rules for custom modules here
-//       // Learn more about loaders from https://webpack.js.org/loaders/
-//     ],
-//   },
-// };
-
-// module.exports = () => {
-//   if (isProduction) {
-//     config.mode = "production";
-
-//     config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-//   } else {
-//     config.mode = "development";
-//   }
-//   return config;
-// };
-
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
@@ -76,10 +15,10 @@ export default {
           },
         },
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -92,6 +31,7 @@ export default {
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
